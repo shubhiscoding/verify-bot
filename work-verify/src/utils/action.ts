2c7a2075391d5be48d7afcd6d7de687889003aa5
Shubh Kesharwani <95687419+shubhiscoding@users.noreply.github.com>
2025-04-23 02:16:22 +0530

    removed SOLANA_RPC_URL log

--- work-verify/src/utils/action.ts
+++ work-verify/src/utils/action.ts
@@ -5,7 +5,6 @@ import { Connection } from "@solana/web3.js";
 export const confirmTransaction = async (signature: string, blockhash: string, lastValidBlockHeight: number)=>{
     try{
         const solConnection = new Connection(process.env.SOLANA_RPC_URL || "https://api.mainnet-beta.solana.com");
-        console.log(process.env.SOLANA_RPC_URL);
         console.log("Confirming transaction with signature:", signature);
         await solConnection.confirmTransaction({
             signature,
